e918071f7f950b410dc64ec67e25bc0ea998350f
sandeepsinghvirdi <sandeep_sonu07@yahoo.com>
2022-07-17 18:06:18 +1000

    Added code for front end

---
+++ src/Components/Mealitem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MealItems = () => {
+    return(
+        <>
+            <div className="card">
+                <img src="https:\/\/www.themealdb.com\/images\/media\/meals\/ustsqw1468250014.jpg" alt=""/>
+                <h3>Spicy Arrabiata Penne</h3>
+            </div>
+        </>
+    )
+}
+
+export default MealItems;
